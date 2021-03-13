31065f6010cbe4f6387e9f8853fcbf5c78445d20
DaniloErazo <erazodanilo1218@gmail.com>
2021-03-12 23:10:34 -0500

    Solution working for several entries but error when trying the debug cases

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,10 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.time.chrono.MinguoChronology;
 import java.util.Arrays;
-
-import com.sun.org.apache.xpath.internal.FoundIndex;
 
 public class Main {
 
@@ -16,53 +13,64 @@ public class Main {
 	private final static BufferedWriter bWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 	
 	public static void main(String[] args) throws IOException {
-		String books = bReader.readLine();
-		int booksNum=Integer.parseInt(books);
-		
-		if(booksNum<2 || booksNum>10000) {
-			bWriter.write("The test case is wrong, check your entry");
-			bWriter.flush();
-		}else {
+		String lineString;
+		String output = "";
+		String books;
+		while((lineString=bReader.readLine())!=null) {
 			
-			String line = bReader.readLine();
-			String[] partStrings = line.split(" ");
-			int[] array = stringArraytoInt(partStrings);
+			if(lineString.equals("")) {
+				books = bReader.readLine();
+			}else {
+				books = lineString;
+			}
+			int booksNum=Integer.parseInt(books);
 			
-			if(array.length!=booksNum) {
+			if(booksNum<2 || booksNum>10000) {
 				bWriter.write("The test case is wrong, check your entry");
 				bWriter.flush();
 			}else {
-			
-				String money = bReader.readLine();
-				int moneyInt = Integer.parseInt(money);
 				
-				Arrays.sort(array);
-				int found = 0;
-				int one = 0, two = 0;
-				int count=0;
-				for (int i = 0; i < array.length ; i++) {
+				String line = bReader.readLine();
+				String[] partStrings = line.split(" ");
+				int[] array = stringArraytoInt(partStrings);
 				
-					int moneySearch = moneyInt-array[i];
-					found = binarySearch(array, moneySearch);
-					if(found>0) {
-						if(array[i]>array[found]) {
-							two = array[i];
-							one = array[found];
-						}else {
-							one=array[i];
-							two=array[found];
+				if(array.length!=booksNum) {
+					bWriter.write("The test case is wrong, check your entry");
+					bWriter.flush();
+				}else {
+				
+					String money = bReader.readLine();
+					int moneyInt = Integer.parseInt(money);
+					
+					Arrays.sort(array);
+					int found = 0;
+					int one = 0, two = 0;
+					int count=0;
+					for (int i = 0; i < array.length ; i++) {
+					
+						int moneySearch = moneyInt-array[i];
+						found = binarySearch(array, moneySearch);
+						if(found>0) {
+							if(array[i]>array[found]) {
+								two = array[i];
+								one = array[found];
+							}else {
+								one=array[i];
+								two=array[found];
+							}
+						} 
+						count++;
+						if(count==booksNum) {
+							output+="Peter should buy books whose prices are "+ one + " and " + two +"\n \n";
 						}
-					} 
-					count++;
-					if(count==booksNum) {
-						bWriter.write("Peter should buy books whose prices are "+ one + " and " + two);
-						bWriter.flush();
 					}
 				}
 			}
+			
 		}
 		
-		
+		bWriter.write(output);
+		bWriter.flush();
 		
 		bReader.close();
 		bWriter.close();
